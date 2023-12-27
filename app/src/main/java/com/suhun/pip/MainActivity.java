ae9046cea13df629b1ff0f9dc2da9894e98012b7
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-12-27 11:16:19 +0800

    init view

--- app/src/main/java/com/suhun/pip/MainActivity.java
+++ app/src/main/java/com/suhun/pip/MainActivity.java
@@ -3,12 +3,26 @@ package com.suhun.pip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
+    private FrameLayout counterFrame;
+    private TextView counterView;
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        counterFrame = findViewById(R.id.lid_counterFrame);
+        counterView = findViewById(R.id.lid_counterView);
+        videoView = findViewById(R.id.lid_videoView);
     }
 }
